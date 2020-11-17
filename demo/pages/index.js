3e2f79a116cfce224f2c0d71ea6e33a4163ff380
Hendrik Human <61615033+jhhuman@users.noreply.github.com>
2020-11-17 14:38:38 +0800

    Update index.js

--- demo/pages/index.js
+++ demo/pages/index.js
@@ -6,39 +6,12 @@ import Link from 'next/link'
 import Date from '../components/date'
 import Image from 'next/image'
 
-export default function Home({ allPostsData }) {
+import Image from 'next/image'
+
+function Home() {
   return (
-    <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section className={utilStyles.headingMd}>
-        <p>
-          Hello, I’m <strong>Shu</strong>. I’m a software engineer and a
-          translator (English/Japanese). You can contact me on{' '}
-          <a href="https://twitter.com/chibicode">Twitter</a>.
-        </p>
-        <p>
-          (This is a sample website - you’ll be building a site like this in{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
-      </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>
-                <a>{title}</a>
-              </Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
-            </li>
-          ))}
-        </ul>
-      </section>
+    <>
+      <h1>My Homepage</h1>
       <Image
         src="/images/profile.jpg"
         alt="Picture of the author"
@@ -46,16 +19,8 @@ export default function Home({ allPostsData }) {
         height={500}
       />
       <p>Welcome to my homepage!</p>
-    </Layout>
-
+    </>
   )
 }
 
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData()
-  return {
-    props: {
-      allPostsData
-    }
-  }
-}
+export default Home
